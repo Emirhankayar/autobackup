6c240a48a073ad5668937856047e2b9d27f351f9
Emirhankayar <emirhan.kayar80@gmail.com>
2023-11-21 14:34:39 +0100

    req

--- worker.py
+++ worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from main import backup
+from main import perform_backup
 import os
 
 # Create a Celery instance
@@ -8,4 +8,4 @@ celery_app = Celery('tasks', broker=os.getenv('CLOUDAMQP_URL'))
 # Define a Celery task that calls the backup function
 @celery_app.task
 def backup_task():
-    backup()
+    perform_backup()
