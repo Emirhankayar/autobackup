d79ecf4e1a9061e881adce8d537dbf11d622b42a
Emirhankayar <emirhan.kayar80@gmail.com>
2023-11-21 14:55:43 +0100

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn -w 4 main:app
+celery -A worker.celery_app worker --loglevel=info
--- worker.py
+++ worker.py
@@ -1,9 +1,17 @@
 from celery import Celery
 from main import perform_backup
 import os
+from celery.schedules import crontab
 
 # Create a Celery instance
 celery_app = Celery('tasks', broker=os.getenv('CLOUDAMQP_URL'))
+
+celery_app.conf.beat_schedule = {
+    'run-every-midnight': {
+        'task': 'worker.backup_task',
+        'schedule': crontab(hour=14, minute=59),
+    },
+}
 
 # Define a Celery task that calls the backup function
 @celery_app.task
